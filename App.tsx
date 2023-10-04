2c49f2eb05f0c50c281be3f9ec78efcf16f64542
TA <somfan3@gmail.com>
2023-10-04 09:27:59 +0700

    update

--- App.tsx
+++ App.tsx
@@ -5,29 +5,30 @@
  * @format
  */
 
-import React from 'react';
+import React from "react";
 import {
   Button,
   SafeAreaView,
   ScrollView,
   StatusBar,
   useColorScheme,
-  View,
-} from 'react-native';
+  View
+} from "react-native";
 
-import {Colors, Header} from 'react-native/Libraries/NewAppScreen';
-import {init, registerDevice} from './module/MySignModule';
+import { Colors, Header } from "react-native/Libraries/NewAppScreen";
+import { init, registerDevice } from "./module/MySignModule";
+
 function App(): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+  const isDarkMode = useColorScheme() === "dark";
 
   const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter
   };
 
   return (
     <SafeAreaView style={backgroundStyle}>
       <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+        barStyle={isDarkMode ? "light-content" : "dark-content"}
         backgroundColor={backgroundStyle.backgroundColor}
       />
       <ScrollView
@@ -36,15 +37,13 @@ function App(): JSX.Element {
         <Header />
         <View
           style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
+            backgroundColor: isDarkMode ? Colors.black : Colors.white
           }}>
           <Button
             title={'test'}
             onPress={async () => {
-              await init('030098003311', '123456789');
-              registerDevice(
-                'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJhc2NlcnRpYSIsInN1YiI6IjAzMDA5ODAwMzMxMSIsImV4cCI6MTY5NjE4OTE4OSwiaWF0IjoxNjk2MTg1NTg5LCJ0b2tlbl9pZCI6ImE1MDMwY2RlLWJlZTMtNDAyYi1iZTY2LWFlZTc2OTMwYTA5ZCIsInNjb3BlIjoiU0NPUEVfUkFTX01PQklMRV9BUFBfT0FVVEgiLCJkZWZhdWx0X3Byb2ZpbGUiOiJ0cnVlIiwiZGVmYXVsdF9jbGllbnQiOiJmYWxzZSIsInJhc19wcm9maWxlIjoiYWRzczpyYXM6cHJvZmlsZTowMDEiLCJ0b2tlbl90eXBlIjoiYWNjZXNzX3Rva2VuIiwiY2xpZW50X2lkIjoic2FtcGxlc190ZXN0X2NsaWVudCJ9.OFAouKblSkWWWgw9Wzw4fsPayGFdECZxs9S_XOOjePk',
-              );
+              await init("030098003311", "123456789");
+              registerDevice("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJhc2NlcnRpYSIsInN1YiI6IjAzMDA5ODAwMzMxMSIsImV4cCI6MTY5NjM2NDE0NiwiaWF0IjoxNjk2MzYwNTQ2LCJ0b2tlbl9pZCI6IjA4ZWVlYjYwLTY1NjgtNDkwOS1iMDFlLWJkMThiNWFkY2Q3NiIsInNjb3BlIjoiU0NPUEVfUkFTX01PQklMRV9BUFBfT0FVVEgiLCJkZWZhdWx0X3Byb2ZpbGUiOiJ0cnVlIiwiZGVmYXVsdF9jbGllbnQiOiJmYWxzZSIsInJhc19wcm9maWxlIjoiYWRzczpyYXM6cHJvZmlsZTowMDEiLCJ0b2tlbl90eXBlIjoiYWNjZXNzX3Rva2VuIiwiY2xpZW50X2lkIjoic2FtcGxlc190ZXN0X2NsaWVudCJ9.KgNWkMBRosJsUhDE1JK2usW1q8XRiKF6iwtd4Wj4wo4");
             }}
           />
         </View>
@@ -52,4 +51,5 @@ function App(): JSX.Element {
     </SafeAreaView>
   );
 }
+
 export default App;
--- module/MySignModule.js
+++ module/MySignModule.js
@@ -3,7 +3,9 @@ import {NativeModules} from 'react-native';
 const {MySignModule} = NativeModules;
 
 export const init = async (userID, deviceID) => {
+  console.log('init');
   await MySignModule.init(userID, deviceID);
+  console.log('init done');
 };
 
 export const registerDevice = authToken => {
